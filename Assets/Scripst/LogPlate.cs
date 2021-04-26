504c658beb33ccf04bb9620f6c8c8c7627bd42c9
Mixail <mixail.korchikov@mail.ru>
2021-04-26 21:58:54 +0300

    Log handler

---
+++ Assets/Scripst/LogPlate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class LogPlate : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textMeshPro;
+    
+    private void Awake()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+     private void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        textMeshPro.text = $"\n [{type}] : {logString} {textMeshPro.text}";
+    }
+}
