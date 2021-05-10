270544c81586de615dc54ae253a43c2a9dea8d6e
Mixail <mixail.korchikov@mail.ru>
2021-05-10 16:03:08 +0300

    Editor Extentions for improve experience

---
+++ Assets/Editor/EditorExtensions.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EditorExtensions : MonoBehaviour
+{
+
+    [MenuItem("TacticMap/Network/StartOnline")]
+    [MenuItem("CONTEXT/NetworkManager/StartOnline")]
+    private static void StartOnlineMatch(MenuCommand command)
+    {
+        NetworkManager networkManager;
+        if (command.context == null)
+        {
+            networkManager = FindObjectOfType<NetworkManager>();
+        }
+        else
+        {
+            networkManager = (NetworkManager)command.context;
+        }
+        networkManager.StartGame(true);
+    }
+
+    [MenuItem("TacticMap/Network/StartOffline")]
+    [MenuItem("CONTEXT/NetworkManager/StartOffline")]
+    private static void StartOfflineMatch(MenuCommand command)
+    {
+        NetworkManager networkManager;
+        if (command.context == null)
+        {
+            networkManager = FindObjectOfType<NetworkManager>();
+        }
+        else
+        {
+            networkManager = (NetworkManager)command.context;
+        }
+        networkManager.StartGame(false);
+    }
+
+    [MenuItem("TacticMap/Network/StopGame")]
+    [MenuItem("CONTEXT/NetworkManager/StopGame")]
+    private static void StopGame(MenuCommand command)
+    {
+        NetworkManager networkManager;
+        if (command.context == null)
+        {
+            networkManager = FindObjectOfType<NetworkManager>();
+        }
+        else
+        {
+            networkManager = (NetworkManager)command.context;
+        }
+        networkManager.StopGame();
+    }
+}
