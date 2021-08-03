ca19ef0c2e5760e470c7a4b733a80bbda2c4fbf2
Mixail <mixail.korchikov@mail.ru>
2021-08-03 14:10:39 +0300

    Improved device support.
    * Menu support for hololens 1 gen.

---
+++ Assets/HololensMenuSupport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class HololensMenuSupport : MonoBehaviour
+{
+    [Header("Hidden Menu")]
+    [SerializeField] private GameObject hiddenMenu;
+
+    [Header("Useless hide button")]
+    [SerializeField] private GameObject hideMenuButton;
+
+    [Header("For Hololens 2 components")]
+    [SerializeField] private MonoBehaviour[] uselessComponents;
+
+    private void Awake()
+    {
+        if (Environment.Is64BitProcess)
+        {
+            hiddenMenu.SetActive(true); 
+            hideMenuButton.SetActive(false);
+            Debug.Log("Is 64 Bit Process");
+            foreach (MonoBehaviour script in uselessComponents)
+            {
+                script.enabled = false;
+            }
+        }
+    }
+}
